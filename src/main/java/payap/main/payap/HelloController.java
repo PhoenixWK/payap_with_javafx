fffafe9f04a9c3d1a5347764c44d76595e4fc3ff
PhoenixWK <xuanphuda@gmail.com>
2025-04-04 11:42:01 +0700

    Acting with ImageView, BorderPane and their stuffs

--- src/main/java/payap/main/payap/HelloController.java
+++ src/main/java/payap/main/payap/HelloController.java
@@ -1,14 +1,12 @@
 package payap.main.payap;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
