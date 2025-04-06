328a634e2ecf84c052287bc4ba1090aaaa8c08b5
xuanphuda <xuanphuda@gmail.com>
2025-04-06 17:29:08 +0700

    Add dashboard header, and containers for total balance, total income, total outcome, weekly summarize, and chart UI

--- src/main/java/payap/main/payap/controller/ui_controlller/DashboardController.java
+++ src/main/java/payap/main/payap/controller/ui_controlller/DashboardController.java
@@ -1,4 +1,20 @@
 package payap.main.payap.controller.ui_controlller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class DashboardController {
+    @FXML
+    private Button cards_btn;
+
+    @FXML
+    private Button dashboard_btn;
+
+    @FXML
+    private Button invoice_btn;
+
+    @FXML
+    private Button transactions_btn;
+
+
 }
